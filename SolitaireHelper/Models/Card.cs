1a9dbc3f8a6b80a69c613cde3f5b7eada0bd14ac
Kasper Baun <kasperbaun@hotmail.com>
2022-03-21 22:07:54 +0100

    Update Card.cs

--- SolitaireHelper/Models/Card.cs
+++ SolitaireHelper/Models/Card.cs
@@ -10,5 +10,40 @@ namespace SolitaireHelper.Models
         public char Suit { get; set; }
         public char Rank { get; set; }
         public bool Visible { get; set; }
-    }
+        //public bool IsRed() { return Suit == Suit.HEARTS || Suit == Suit.DIAMONDS; }
+
+        //public bool IsBlack() { return Suit == Suit.SPADES || Suit == Suit.CLUBS; }
+
+        public string ToString()
+        {
+            return Suit.ToString() + " " ;
+        }
+
+    
+        }
+
+        public enum Suit
+        {
+            HEARTS = 1,
+            CLUBS = 2,
+            DIAMONDS = 3,
+            SPADES = 4
+        }
+        public enum FaceValue
+        {
+            NULL = 0,
+            ONE = 1,
+            TWO = 2,
+            THREE = 3,
+            FOUR = 4,
+            FIVE = 5,
+            SIX = 6,
+            SEVEN = 7,
+            EIGHT = 8,
+            NINE = 9,
+            TEN = 10,
+            KNIGHT = 11,
+            QUEEN = 12,
+            KING = 13
+        }
 }
