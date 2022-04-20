a7478eae440e2e6754e02dda1b2ea0ce185edcfc
Kasper Baun <kasperbaun@hotmail.com>
2022-04-20 14:57:00 +0200

    Update GameTest.cs

--- SolitaireHelperModels/GameTest.cs
+++ SolitaireHelperModels/GameTest.cs
@@ -21,8 +21,21 @@ namespace SolitaireHelperModels
         public int RunGame()
         {
             CardDeck CardDeck = new CardDeck();
-            Table = new Table(CardDeck.Deck);
-            
+            CardDeck.ShuffleDeck();
+            Pile F1 = new Pile();
+            Pile F2 = new Pile();
+            Pile F3 = new Pile();
+            Pile F4 = new Pile();
+            Pile T1 = new Pile(); T1.PushCards(CardDeck.Deck.GetRange(0, 1));
+            Pile T2 = new Pile(); T2.PushCards(CardDeck.Deck.GetRange(1, 2));
+            Pile T3 = new Pile(); T3.PushCards(CardDeck.Deck.GetRange(3, 3));
+            Pile T4 = new Pile(); T4.PushCards(CardDeck.Deck.GetRange(6, 4));
+            Pile T5 = new Pile(); T5.PushCards(CardDeck.Deck.GetRange(10, 5));
+            Pile T6 = new Pile(); T6.PushCards(CardDeck.Deck.GetRange(15, 6));
+            Pile T7 = new Pile(); T7.PushCards(CardDeck.Deck.GetRange(21, 7));
+            Pile Talon = new Pile(); Talon.PushCards(CardDeck.Deck.GetRange(28, 3));
+            Pile Stock = new Pile(); Stock.PushCards(CardDeck.Deck.GetRange(31, 52));
+            Table = new Table(T1, T2, T3, T4, T5, T6, T7, F1, F2, F3, F4, Talon, Stock );
 
             return 0;
         }
