44934cede385ab20300e928fb1e24e2dfd6c9d54
Annika Ehlers <e_hlers@live.dk>
2022-06-15 09:41:28 +0200

    Create separate project for ObjectDetection

---
+++ ObjectDetection/ObjectDetection/ObjectDetection.cs
@@ -0,0 +1,21 @@
+﻿using System.Drawing;
+using System.Drawing.Drawing2D;
+using YoloParser;
+using DataStructures;
+using Microsoft.ML;
+
+namespace SolitaireHelper.ObjectDetection
+{
+    public class ObjectDetection
+    {
+        public string GetAbsolutePath(string relativePath)
+        {
+            FileInfo _dataRoot = new FileInfo(typeof(Program).Assembly.Location);
+            string assemblyFolderPath = _dataRoot.Directory.FullName;
+
+            string fullPath = Path.Combine(assemblyFolderPath, relativePath);
+
+            return fullPath;
+        }
+    }
+}
